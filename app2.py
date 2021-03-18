69531bd31fc9b47bfb81ab90f6cb88cac9f338f4
nich02 <nichseemail@gmail.com>
2021-03-18 14:38:20 +0300

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from flask import Flask, jsonify, make_response, request, abort
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open("savmodel.pkl", "rb"))
+
+@app.route("/")
+def hello():
+  return render_template('index2.html')
+
+@app.route("/get_prediction", methods=['POST','OPTIONS'])
+#@cross_origin()
+def get_prediction():
+    if not request.json:
+        abort(400)
+    df = pd.DataFrame(request.json, index2=[0])
+    cols=["Store","Dept","IsHoliday"]
+    df = df[cols]
+    return jsonify({'result': model.predict(df)[0]}), 201
+
+if __name__ == "__main__":
+  app.run()
---
+++ model.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import pickle
+#import matplotlib.pyplot as plt 
+dataset = pd.read_csv("C:\\Users\\Donatus\\Documents\\WalmartPredict\\WalmartSales.csv")
+dataset.isnull().sum()
+
+#dataset['rate'].fillna(0, inplace=True)
+
+#dataset['sales_in_first_month'].fillna(dataset['sales_in_first_month'].mean(), inplace=True)
+
+#X = dataset.iloc[:, :3]
+
+#date time
+df=dataset.copy()
+pd.to_datetime(df['Date'])
+
+df = df.drop('Date', 1)
+#convert to numericals
+df['IsHoliday']=df['IsHoliday'].apply(lambda x: 0 if x== 'FALSE' else 0)
+convert=False
+
+X = df[['Store', 'Dept', 'IsHoliday']] 
+
+y = df['Weekly_Sales']
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=.25, random_state=42)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+#Saving the model as pickle file
+pickle.dump(regressor, open('savmodel.pkl','wb'))
+model = pickle.load(open('savmodel.pkl','rb'))
+#print(model.predict([[4, 300, 500]]))
+
+preds = model.predict(x_test)
+
+from sklearn.metrics import r2_score
+
+print(f'Your model scores {r2_score(preds, y_test)} on test set')
